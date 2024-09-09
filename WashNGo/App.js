2865c94d99856e33dfed15805222a9cd6a4acf2b
vk-develops <vimalkumarv291@gmail.com>
2024-09-09 22:42:51 +0530

    added the onboarding screen

--- WashNGo/App.js
+++ WashNGo/App.js
@@ -1,10 +1,22 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Appstack from "./src/Navigation/Appstack";
 
 export default function App() {
+    const [fontsLoaded] = useFonts({
+        jakartaRegular: require("./assets/fonts/PlusJakartaSans-Regular.ttf"),
+        jakartaMedium: require("./assets/fonts/PlusJakartaSans-Medium.ttf"),
+        jakartaSemiBold: require("./assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+        jakartaBold: require("./assets/fonts/PlusJakartaSans-Bold.ttf"),
+    });
+
+    if (!fontsLoaded) {
+        return null;
+    }
+
     return (
         <SafeAreaView style={styles.container}>
             <StatusBar style="auto" />
--- WashNGo/src/Screens/OnboardingScreen.js
+++ WashNGo/src/Screens/OnboardingScreen.js
@@ -8,31 +8,63 @@ import {
 } from "react-native";
 import React from "react";
 import Logo from "../../assets/Images/Logo.png";
+import MediumBlob from "../../assets/Images/Mediumblob.png";
 
 const width = Dimensions.get("window").width;
 
 const OnboardingScreen = () => {
     return (
-        <View style={{ flex: 1, padding: 16 }}>
+        <View
+            style={{
+                flex: 1,
+                backgroundColor: "#fff",
+                position: "relative",
+            }}
+        >
+            <View style={{ position: "absolute", top: 0, right: -10 }}>
+                <Image
+                    source={MediumBlob}
+                    style={{
+                        width: 150,
+                        resizeMode: "contain",
+                        height: 150,
+                        zIndex: 1,
+                    }}
+                />
+            </View>
             <View
                 style={{
                     flex: 0.6,
                     alignItems: "center",
                     justifyContent: "center",
+                    paddingHorizontal: 16,
                 }}
             >
                 <Image
                     source={Logo}
-                    style={{ height: "85%", width, resizeMode: "contain" }}
+                    style={{
+                        height: "85%",
+                        width,
+                        resizeMode: "contain",
+                        marginTop: 75,
+                    }}
                 />
             </View>
-            <View style={{ flex: 0.4 }}>
+            <View
+                style={{
+                    flex: 0.4,
+                    paddingHorizontal: 16,
+                    paddingTop: 35,
+                }}
+            >
                 <Text
                     style={{
                         fontSize: 24,
-                        fontWeight: "black",
+
                         textAlign: "center",
                         marginBottom: 50,
+                        fontFamily: "jakartaSemiBold",
+                        color: "#1e1e1e",
                     }}
                 >
                     Sparkle & Shine Transform Your Drive with Every Wash!
@@ -43,12 +75,14 @@ const OnboardingScreen = () => {
                         backgroundColor: "#A3CFFF",
                         borderRadius: 100,
                         marginBottom: 25,
+                        borderColor: "#94C7FF",
+                        borderWidth: 2,
                     }}
                 >
                     <Text
                         style={{
-                            fontSize: 24,
-                            fontWeight: "black",
+                            fontSize: 20,
+                            fontFamily: "jakartaSemiBold",
                             textAlign: "center",
                             paddingVertical: 12,
                         }}
@@ -62,6 +96,7 @@ const OnboardingScreen = () => {
                         alignItems: "center",
                         justifyContent: "center",
                         flexDirection: "row",
+                        gap: 3,
                     }}
                 >
                     <Text
@@ -69,6 +104,8 @@ const OnboardingScreen = () => {
                             fontSize: 16,
                             fontWeight: "black",
                             textAlign: "center",
+                            fontFamily: "jakartaMedium",
+                            color: "#555",
                         }}
                     >
                         Already have an account?
@@ -79,6 +116,9 @@ const OnboardingScreen = () => {
                                 fontSize: 16,
                                 fontWeight: "black",
                                 textAlign: "center",
+                                fontFamily: "jakartaSemiBold",
+                                color: "#222",
+                                paddingBottom: -2,
                             }}
                         >
                             Sign In
